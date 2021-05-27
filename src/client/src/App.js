39b61808003af06d7b65d5709b577aa587ece0b9
meddlin <drushing.dev@gmail.com>
2021-05-26 21:12:37 -0500

    front-end can display data from back-end
    - Call to server, which calls database, which all returns data, and is displayed

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,31 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ someData, setSomeData ] = useState({});
 
   const callServer = async () => {
     const response = await fetch('/api/getData');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
 
-    // this.setState({ renderedResponse: someData });
     console.table(body);
-    return { renderedResponse: body };
+    setSomeData(body);
   };
 
   return (
     <div className="App">
       <h2>App Test</h2>
 
-      <div className="App">
-        <h2>Call out to API!</h2>
-        {/* <p>{renderedResponse.express}</p> */}
+      <div>
+        <h3>Call out to API!</h3>
+        <p>
+          From the server: <b>{someData && someData[0] && someData[0].someKey ? someData[0].someKey : ''}</b>
+        </p>
       </div>
 
       <button onClick={() => callServer()}>Click Me</button>
 
-      <ul>
+      {/* <ul>
         <li>test data goes here</li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
